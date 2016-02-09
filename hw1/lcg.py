37b5f3eb874a826ae29b49347bf593119b4dc017
Ishola Babatunde Isaac <tecbossmb@gmail.com>
2016-02-09 12:37:22 -0600

    fixed indentation issues

--- hw1/lcg.py
+++ hw1/lcg.py
@@ -9,65 +9,65 @@ from scipy import stats
 #lcg uses simple linear congruential generator to create n random numbers
 
 def init():
-    global M
-    M = 2048
+	global M
+	M = 2048
 
 def seed(value):
-    #set seed value
+	#set seed value
 	global x_i
 	x_i = float(value)
 
 # linear congruential generator   
 def lcg():
-    # define lcg specific parameters
+	# define lcg specific parameters
 	a = 1229
 	c = 1
-	
+
 	global x_i
 	x_i = mod(a*x_i + c, M)
 	return x_i
 
 #generate n random series using lcg
 def lcg_randoms(seedValue, n):
-    seed(seedValue)
-    x = []
-    #x.append(x_i)
-    for i in range(n):
-        x.append(lcg())
-    return x
+	seed(seedValue)
+	x = []
+	#x.append(x_i)
+	for i in range(n):
+		x.append(lcg())
+	return x
 
 # extract x and y alternating points    
 def split_data(data):
-    x = data[::2]
-    y = data[1::2]
-    
-    return x, y
-    
+	x = data[::2]
+	y = data[1::2]
+
+	return x, y
+
 # generate scaled x and y data    
 def generate_data(s, N):
-    series = lcg_randoms(s, N)
-    [x, y] = split_data(series)
-    scaled_x = [x/M for x in x]
-    scaled_y = [y/M for y in y]
-    return scaled_x, scaled_y
+	series = lcg_randoms(s, N)
+	[x, y] = split_data(series)
+	scaled_x = [x/M for x in x]
+	scaled_y = [y/M for y in y]
+	return scaled_x, scaled_y
 
 #make plot
 def plotxy(x, y, title):
 	plt.figure(title)
 	plt.plot(x,y,'bo')
 	plt.show()
-	
-	
+
+
 # entry point    
 def main():
-    init()
+	init()
 	seedValue = 567
 	N = 2000
-    [x, y] = generate_data(seedValue, N)
-    #prints the covariance of x,y
-    print 'covariance of x and y <= simple linear congruential generator:'
-    print(cov(x,y))
-    plotxy(x,y,'q1-lcg')
-    
-	
+	[x, y] = generate_data(seedValue, N)
+	#prints the covariance of x,y
+	print 'covariance of x and y <= simple linear congruential generator:'
+	print(cov(x,y))
+	plotxy(x,y,'q1-lcg')
+
+
 main() 
