2b140f0549d484280888da89af332b05bc78adb0
techbossmb <tecbossmb@gmail.com>
2016-02-09 16:33:21 -0600

    added txt result and fixed a few indentation

--- hw1/py_random.py
+++ hw1/py_random.py
@@ -40,8 +40,8 @@ def main():
 	[x, y] = generate_data(N)
 	#prints the covariance of x,y
 	out = open('q3.txt', 'w')
-    	print >>out, 'covariance of x and y \n for py .random():'
-    	print >>out, cov(x,y)
+    print >>out, 'covariance of x and y \n for py .random():'
+    print >>out, cov(x,y)
 	plotxy(x,y,'q3-py_random')
 
 
