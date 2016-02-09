ece4ab9cbcafbcd7d23e1744c1d5660641d9c066
Ishola Babatunde Isaac <tecbossmb@gmail.com>
2016-02-09 12:40:02 -0600

    fixed indentation issues

--- hw1/py_random.py
+++ hw1/py_random.py
@@ -11,38 +11,38 @@ from random import random
 
 #generate n random series using py random()
 def py_randoms(n):
-    series = zeros([n,1])
-    return [random() for series in series]
+	series = zeros([n,1])
+	return [random() for series in series]
 
 # extract x and y alternating points    
 def split_data(data):
-    x = data[::2]
-    y = data[1::2]
-    return x, y
-    
+	x = data[::2]
+	y = data[1::2]
+	return x, y
+
 # generate scaled x and y data    
 def generate_data(N):
-    series = py_randoms(N)
-    [x, y] = split_data(series)
-    return x, y
+	series = py_randoms(N)
+	[x, y] = split_data(series)
+	return x, y
 
 #make plot
 def plotxy(x, y, title):
 	plt.figure(title)
 	plt.plot(x,y,'bo')
 	plt.show()
-	
-	
+
+
 # entry point    
 def main():
 	# generate 2000 random numbers
 	N = 2000
-    [x, y] = generate_data(N)
-    #prints the covariance of x,y
-    print 'covariance of x and y <= py .random():'
-    print(cov(x,y))
-    plotxy(x,y,'q3-py_random')
-    
-	
+	[x, y] = generate_data(N)
+	#prints the covariance of x,y
+	print 'covariance of x and y <= py .random():'
+	print(cov(x,y))
+	plotxy(x,y,'q3-py_random')
+
+
 main() 
 
