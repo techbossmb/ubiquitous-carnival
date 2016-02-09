2f5ccb3d6716e4462f53ba32194855c33ca9e5fc
Ishola Babatunde Isaac <tecbossmb@gmail.com>
2016-02-09 12:46:08 -0600

    fixed indentation issues

--- hw1/shuffled_lcg.py
+++ hw1/shuffled_lcg.py
@@ -9,33 +9,33 @@ from scipy import stats
 #shuffled version of linear congruential generator
 
 def initM(argument=None):
-    global M
-    M = 2048
-    if argument is not None:
+	global M
+	M = 2048
+	if argument is not None:
 		M = argument
-		
+
 def seed(value):
-    #set seed value
+	#set seed value
 	global x_i
 	x_i = value
 
 # linear congruential generator   
 def lcg():
-    # define lcg specific parameters
+	# define lcg specific parameters
 	a = 1229
 	c = 1
-	
+
 	global x_i
 	x_i = mod(a*x_i + c, M)
 	return x_i
 
 #generate n random series using lcg
 def lcg_randoms(seedValue, n):
-    seed(seedValue)
-    series = []
-    for i in range(n):
-        series.append(lcg())
-    return series
+	seed(seedValue)
+	series = []
+	for i in range(n):
+		series.append(lcg())
+	return series
 
 #shuffle n random numbers from N lcg_random numbers using T table with s = seed value
 def shuffle(s, n, N):
@@ -43,7 +43,7 @@ def shuffle(s, n, N):
 	x = lag_series(series, 0, N)
 	y = lag_series(series, 1, N)
 	z = lag_series(series, 2, N)
-	
+
 	#needed to revert M to float
 	initM(float(M)) 
 	#L: table T of L values 
@@ -59,43 +59,43 @@ def shuffle(s, n, N):
 		T[ z[k+1]%L ] = (x[k+L+3] + (y[k+L+1])/M )/M
 		w.append(w_k)
 	return w
-	
+
 # allows for z_0 = y_1 = x_2
 def lag_series(series, lag, length):
 	return series[lag:length+lag]
-	
+
 # extract x and y alternating points    
 def split_data(data):
-    x = data[::2]
-    y = data[1::2]
-    return x, y
-    
+	x = data[::2]
+	y = data[1::2]
+	return x, y
+
 # generate scaled x and y data given seed value = s 
 def generate_data(s, n, N):
-    series = shuffle(s, n, N)
-    #print series
-    [x, y] = split_data(series)
-    scaled_x = [x for x in x]
-    scaled_y = [y for y in y]
-    return scaled_x, scaled_y
-    
+	series = shuffle(s, n, N)
+	#print series
+	[x, y] = split_data(series)
+	scaled_x = [x for x in x]
+	scaled_y = [y for y in y]
+	return scaled_x, scaled_y
+
 
 #make plot
 def plotxy(x, y, title):
 	plt.figure(title)
 	plt.plot(x,y,'bo')
 	plt.show()
-	
-	
+
+
 # entry point    
 def main():
-    initM()
+	initM()
 	# generate 2000 random numbers from 5000 lcg generated random number with seed value = 457
-    [x, y] = generate_data(457, 2000, 5000)
-    #prints the covariance of x,y
-    print 'covariance of x and y <= shuffled lcg:'
-    print(cov(x,y))
-    plotxy(x,y,'q2-shuffled_lcg')
-	
+	[x, y] = generate_data(457, 2000, 5000)
+	#prints the covariance of x,y
+	print 'covariance of x and y <= shuffled lcg:'
+	print(cov(x,y))
+	plotxy(x,y,'q2-shuffled_lcg')
+
 main() 
 
