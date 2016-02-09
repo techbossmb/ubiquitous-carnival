113d4a2a3f91255b1f744964c08af4ae83128b36
techbossmb <tecbossmb@gmail.com>
2016-02-08 23:40:20 -0600

    added questions 2 and 3

---
+++ py_random.py
@@ -0,0 +1,46 @@
+from numpy import *
+import time
+import matplotlib.pyplot as plt
+import pylab
+from scipy import stats
+from random import random
+
+#@author: Ishola Babatunde
+#@date: 02/08/16
+#@random number generator uses python random() function
+
+#generate n random series using py random()
+def py_randoms(n):
+    series = zeros([n,1])
+    return [random() for series in series]
+
+# extract x and y alternating points    
+def split_data(data):
+    x = data[::2]
+    y = data[1::2]
+    return x, y
+    
+# generate scaled x and y data    
+def generate_data(N):
+    series = py_randoms(N)
+    [x, y] = split_data(series)
+    return x, y
+
+#plot
+def plotxy(x, y, title):
+	plt.figure(title)
+	plt.plot(x,y,'bo')
+	plt.show()
+	
+	
+# entry point    
+def main():
+    [x, y] = generate_data(2000)
+    #prints the covariance of x,y
+    print 'covariance of x and y <= py .random():'
+    print(cov(x,y))
+    plotxy(x,y,'q3-py_random')
+    
+	
+main() 
+
