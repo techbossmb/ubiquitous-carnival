cba88ac3531c5f89c041603638f73004e41fe416
techbossmb <techbossmb@localhost.(none)>
2016-02-10 14:23:10 -0600

    fixed minor issues

--- hw1/lcg.py
+++ hw1/lcg.py
@@ -9,67 +9,65 @@ from scipy import stats
 #lcg uses simple linear congruential generator to create n random numbers
 
 def init():
-	global M
-	M = 2048
+    global M
+    M = 2048
 
 def seed(value):
-	#set seed value
+    #set seed valuegit 
 	global x_i
 	x_i = float(value)
 
 # linear congruential generator   
 def lcg():
-	# define lcg specific parameters
+    # define lcg specific parameters
 	a = 1229
 	c = 1
-
+	
 	global x_i
 	x_i = mod(a*x_i + c, M)
 	return x_i
 
 #generate n random series using lcg
 def lcg_randoms(seedValue, n):
-	seed(seedValue)
-	x = []
-	#x.append(x_i)
-	for i in range(n):
-		x.append(lcg())
-	return x
+    seed(seedValue)
+    x = []
+    #x.append(x_i)
+    for i in range(n):
+        x.append(lcg())
+    return x
 
 # extract x and y alternating points    
 def split_data(data):
-	x = data[::2]
-	y = data[1::2]
-
-	return x, y
-
+    x = data[::2]
+    y = data[1::2]
+    
+    return x, y
+    
 # generate scaled x and y data    
-def generate_rv(s, N):
-	series = lcg_randoms(s, N)
-	[x, y] = split_data(series)
-	scaled_x = [x/M for x in x]
-	scaled_y = [y/M for y in y]
-	return scaled_x, scaled_y
+def generate_data(s, N):
+    series = lcg_randoms(s, N)
+    [x, y] = split_data(series)
+    scaled_x = [x/M for x in x]
+    scaled_y = [y/M for y in y]
+    return scaled_x, scaled_y
 
-#make plot
+#plot
 def plotxy(x, y, title):
 	plt.figure(title)
 	plt.plot(x,y,'bo')
 	plt.show()
-
-
+	
+	
 # entry point    
 def main():
-	init()
-	seedValue = 567
-	N = 2000
-	[x, y] = generate_rv(seedValue, N)
-	#prints the covariance of x,y
-	out = open('q1.txt', 'w')
-	print >>out, 'covariance of x and y \n for simple linear congruential generator:'
-	print >>out, cov(x,y)
-	out.close()
-	plotxy(x,y,'q1-lcg')
-
-if __name__ == '__main__':
+    init()
+    [x, y] = generate_data(567, 2000)
+    #prints the covariance of x,y
+    out = open('result/q1.txt', 'w')
+    print >>out, 'covariance of x and y \n for simple linear congruential generator:'
+    print >>out, cov(x,y)
+    out.close()
+    plotxy(x,y,'q1-lcg')
+    
+if __name__ == '__main__':	
 	main() 
--- hw1/py_random.py
+++ hw1/py_random.py
@@ -11,39 +11,38 @@ from random import random
 
 #generate n random series using py random()
 def py_randoms(n):
-	series = zeros([n,1])
-	return [random() for series in series]
+    series = empty([n,1])
+    return [random() for series in series]
 
 # extract x and y alternating points    
 def split_data(data):
-	x = data[::2]
-	y = data[1::2]
-	return x, y
-
+    x = data[::2]
+    y = data[1::2]
+    return x, y
+    
 # generate scaled x and y data    
 def generate_data(N):
-	series = py_randoms(N)
-	[x, y] = split_data(series)
-	return x, y
+    series = py_randoms(N)
+    [x, y] = split_data(series)
+    return x, y
 
-#make plot
+#plot
 def plotxy(x, y, title):
 	plt.figure(title)
 	plt.plot(x,y,'bo')
 	plt.show()
-
-
+	
+	
 # entry point    
 def main():
-	# generate 2000 random numbers
-	N = 2000
-	[x, y] = generate_data(N)
-	#prints the covariance of x,y
-	out = open('q3.txt', 'w')
-	print >>out, 'covariance of x and y \n for py .random():'
-	print >>out, cov(x,y)
-	plotxy(x,y,'q3-py_random')
-
-if __name__ == '__main__':
+    [x, y] = generate_data(2000)
+    #prints the covariance of x,y
+    out = open('result/q3.txt', 'w')
+    print >>out, 'covariance of x and y \n for py .random():'
+    print >>out, cov(x,y)
+    out.close()
+    plotxy(x,y,'q3-py_random')
+    
+if __name__ =='__main__':	
 	main() 
 
