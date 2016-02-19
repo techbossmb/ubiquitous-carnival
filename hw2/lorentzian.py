d7024f185b8f2b22011fb00dc22fe5f74495fa98
techbossmb <techbossmb@localhost.(none)>
2016-02-18 19:55:20 -0600

    added comments

--- hw2/lorentzian.py
+++ hw2/lorentzian.py
@@ -9,10 +9,12 @@ from scipy.integrate import quad
 #@date: 02/16/16
 #@ lorentzian distribution from gamma distribution
 
+#initialize parameters of the lorentzian distribution
 def initVars():
 	global a0, c0, x0
-	a0, c0, x0 = 1, 3, 3
+	a0, c0, x0 = 1, 5, 3
 	
+#evaluate the gamma distribution - defined only for +ve values
 def gamma_dist(x):
 	n = 5
 	if x <= 0:
@@ -21,18 +23,22 @@ def gamma_dist(x):
 		g_x = (x**(n-1)*exp(-x))/24
 	return g_x
 
+#evaluate lorentzian distribution
 def lorentzian(x):
 	f_x = (c0/(1+ (x - x0)**2)/a0)
 	return f_x
 
+#calculate the integrand, integrating from zero to +ve infinity
 def integral_fx():
 	maxValue, err = quad(lorentzian, 0, Inf)
 	return maxValue
 
+#calculate the g_of_z
 def calculateXGivenZ(z):
 	_3root1 = c0*sqrt(a0)
 	return c0 + (sqrt(a0) * tan(z/_3root1))
 	
+#calculate mean and variance of the distribution
 def calculate_stats(z):
 	#calculate mean
 	ev = mean(z)
@@ -41,11 +47,13 @@ def calculate_stats(z):
 	variance = var(z)
 	print 'Variance: ' + str(variance)
 	
+#entry point
+#does the lorentzian distribution using the rejection method over the gamma distribution
 def main():
 	accepted = []
 	initVars()
 	A = integral_fx()
-	print 'Area under the curve = '+A
+	print 'Area under the curve = '+ str(A)
 	for i in range(10000):
 		u1 = random.uniform(0, A)
 		x = calculateXGivenZ(u1)
@@ -59,8 +67,9 @@ def main():
 	plt.savefig('result/lorentzian')
 	calculate_stats(accepted)
 	print 'accepted '+str(size(accepted))+' out of 10000'
+	print 'rejected '+str(10000-size(accepted))+' out of 10000'
 	plt.show()
-git o
+	
 if __name__=='__main__':
 	main()
 
