46ba5895a65369f6d53697cddeeadae49cfe212e
techbossmb <techbossmb@localhost.(none)>
2016-02-25 21:33:40 -0600

    hw 3 q. 1 first commit

---
+++ hw3/lcg.py
@@ -0,0 +1,80 @@
+from numpy import *
+import time
+import matplotlib.pyplot as plt
+import pylab
+from scipy import stats
+
+#@author: Ishola Babatunde
+#@date: 02/08/16
+#lcg uses simple linear congruential generator to create n random numbers
+
+def init():
+    global M
+    M = 2048
+
+def seed(value):
+    #set seed valuegit 
+	global x_i
+	x_i = float(value)
+
+# linear congruential generator   
+def lcg():
+    # define lcg specific parameters
+	a = 1229
+	c = 1
+	
+	global x_i
+	x_i = mod(a*x_i + c, M)
+	return x_i
+
+#generate n random series using lcg
+def lcg_randoms(seedValue, n):
+    seed(seedValue)
+    x = []
+    #x.append(x_i)
+    for i in range(n):
+        x.append(lcg())
+    return x
+
+# extract x and y alternating points    
+def split_data(data):
+    x = data[::2]
+    y = data[1::2]
+    
+    return x, y
+    
+# generate scaled x and y data    
+def generate_multivariate_data(s, N):
+    series = lcg_randoms(s, N)
+    [x, y] = split_data(series)
+    scaled_x = [x/M for x in x]
+    scaled_y = [y/M for y in y]
+    return scaled_x, scaled_y
+    
+def generate_univariate_data(s, N):
+	series = lcg_randoms(s, N)
+	scaled_data = [series/M for series in series]
+	return scaled_data
+
+#plot
+def plotxy(x, y, title):
+	plt.figure(title)
+	plt.xlabel('x')
+	plt.ylabel('y')
+	fig = plt.plot(x,y, 'bo')
+	plt.savefig('result/'+title)
+	
+	
+def main():
+    init()
+    [x, y] = generate_multivariate_data(567, 2000)
+    #prints the covariance of x,y to file
+    # you might have to create result folder if it doesn't exist else this line throw exception
+    out = open('result/q1.txt', 'w')
+    print >>out, 'covariance of x and y \n for simple linear congruential generator:'
+    print >>out, cov(x,y)
+    out.close()
+    plotxy(x,y,'q1-lcg')
+    
+if __name__ == '__main__':	
+	main()
---
+++ hw3/lcg_confidence.py
@@ -0,0 +1,49 @@
+from numpy import *
+import matplotlib.pyplot as plt
+import pylab
+from scipy import stats
+import lcg
+
+#@author: Ishola Babatunde
+#@ques: determine the number of samples need to achieve a 99% CI for LCG generated rv
+
+# initialize CI=>zscore map
+def initMap():
+	global ci_map
+	ci_map = {'99%':2.576, '95%':1.96, '90%':1.645} 
+	
+#generate uniform rv from linear congruential generator
+#args: n = number of random variables to generate
+def do_lcg(n):
+	lcg.init()
+	rv = lcg.generate_univariate_data(345, n)
+	return rv
+
+#returns the sample size for need to achieve a given confidence interval
+def get_sample_size(margin_error, confidence_interval, expected_value):
+	initMap()
+	z_score = ci_map[confidence_interval]
+	#ME = zScore * \sqrt{\frac{EV(1-EV)}{n}}
+	#make n the subject of the formula in the equation above to derive the equation for sample_size
+	sample_size = (expected_value * (1 - expected_value))/((margin_error/z_score)**2)
+	return sample_size
+
+def calculata_stats(rv):
+	return mean(rv), var(rv)
+
+def plot_hist(data):
+	plt.figure('lcg-generated uniform rv')
+	plt.hist(data)
+	plt.show()
+
+#entry point
+def main():
+	margin_error, confidence_interval, expected_value = 0.01, '99%', 0.5
+	sample_size = get_sample_size(margin_error, confidence_interval, expected_value)
+	uniform_rv = do_lcg(int(sample_size))
+	[mu, sigma] = calculata_stats(uniform_rv)
+	print mu, sigma
+	plot_hist(uniform_rv)
+
+if __name__=='__main__':
+	main()
