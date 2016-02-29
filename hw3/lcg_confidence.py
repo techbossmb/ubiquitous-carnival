b863be8306df598cd15505d345a4bddccaddcad6
techbossmb <techbossmb@localhost.(none)>
2016-02-28 20:14:02 -0600

    still working on this

--- hw3/lcg_confidence.py
+++ hw3/lcg_confidence.py
@@ -2,6 +2,7 @@ from numpy import *
 import matplotlib.pyplot as plt
 import pylab
 from scipy import stats
+from scipy.integrate import quad
 import lcg
 
 #@author: Ishola Babatunde
@@ -19,6 +20,7 @@ def do_lcg(n):
 	rv = lcg.generate_univariate_data(345, n)
 	return rv
 
+
 #returns the sample size for need to achieve a given confidence interval
 def get_sample_size(margin_error, confidence_interval, expected_value):
 	initMap()
@@ -28,13 +30,16 @@ def get_sample_size(margin_error, confidence_interval, expected_value):
 	sample_size = (expected_value * (1 - expected_value))/((margin_error/z_score)**2)
 	return sample_size
 
+
 def calculata_stats(rv):
 	return mean(rv), var(rv)
 
-def goodness_of_fit_uniform_rv(rv, ev):
-	ev_list = ev*ones(size(rv))
-	[xsquare, p_value] = stats.chisquare(rv, f_exp=ev_list)
-	return xsquare, p_value
+
+#def goodness_of_fit_uniform_rv(rv, ev):
+#	ev_list = ev*ones(size(rv))
+#	[xsquare, p_value] = stats.chisquare(rv, f_exp=ev_list)
+#	return xsquare, p_value
+
 
 def plot_hist(data):
 	title = 'lcg_generated_uniform rv'
@@ -43,16 +48,51 @@ def plot_hist(data):
 	plt.savefig('result/'+title)
 	plt.show()
 
-#entry point
-def main():
+# x squared goodness of fit test using chi-square
+def testing_chi(rvs, number_of_bins, integrand):
+	sorted_data = sort(rvs)
+	number_of_bins=10;        
+	bin_ends=linspace(0,1,number_of_bins+1) #setp up bin boundaries
+	count=zeros(number_of_bins)
+	p=zeros(number_of_bins)
+	for k in range(number_of_bins):
+		temp=where((sorted_data > bin_ends[k]) & (sorted_data < bin_ends[k+1])) #gives indices of rvs that fall w/in each bin
+		count[k]=size(temp) #counts indices
+		p[k], err=quad(integrand,bin_ends[k],bin_ends[k+1]) #calculates probabily of rv falling w/in each bin
+	n = len(rvs)
+	chi_squared = [(count[k] - n/number_of_bins) ** 2 for k in range(1, number_of_bins)]
+	ans = number_of_bins/n * sum(chi_squared)
+	return ans
+
+
+#test lcg goodness of fit
+def test_lcg():
 	margin_error, confidence_interval, expected_value = 0.01, '99%', 0.5
 	sample_size = get_sample_size(margin_error, confidence_interval, expected_value)
 	uniform_rv = do_lcg(int(sample_size))
-	[mu, sigma] = calculata_stats(uniform_rv)
-	print mu, sigma
+	number_of_bins = 10
+	integrand = lambda x : 1
+	actual = testing_chi(uniform_rv, number_of_bins, integrand)
+	expected = stats.chi2.ppf(.99, number_of_bins-1)
+	evaluate_results(actual, expected)
+
+
+def evaluate_results(actual, expected):
+	print("Actual: ", actual, " Expected: ", expected)
+	if (actual > expected):
+		print("Reject the null hypothesis! RNG poorly fits target")
+	else:
+		print("Fail to reject!")
+
+
+#entry point
+def main():
+	test_lcg()
+	#[mu, sigma] = calculata_stats(uniform_rv)
+	#print mu, sigma
 	#[xsquare, p_value] = goodness_of_fit_uniform_rv(uniform_rv, expected_value)
 	#print xsquare, p_value
-	plot_hist(uniform_rv)
+	#plot_hist(uniform_rv)
 
 if __name__=='__main__':
 	main()
