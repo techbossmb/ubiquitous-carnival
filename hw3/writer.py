e16c68b2bebbd14f77da35776b5e638dae53b975
techbossmb <techbossmb@localhost.(none)>
2016-03-04 11:56:35 -0600

    added readme

--- hw3/writer.py
+++ hw3/writer.py
@@ -1,8 +1,10 @@
 #file helper functions
+
+#write the 
 def write(string):
 	with open('result/result.txt', 'w+') as file:
-		print >>file, string
+		print >>file, string #redirect print output to file
 
 def append(string):
 	with open('result/result.txt', 'a') as file:
-		print >>file, string
+		print >>file, string #redirect print output to file
