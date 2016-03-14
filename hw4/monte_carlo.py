44d4462978b20987d5bfda0f997845bd1191a934
techbossmb <isholabi@gmail.com>
2016-03-13 22:51:53 -0500

    question 2 random walk

--- hw4/monte_carlo.py
+++ hw4/monte_carlo.py
@@ -17,10 +17,10 @@ def run_problem1(sample_size):
 def run_problem2(sample_size):
 	u = zeros([sample_size, 1])
 	u = [random.random() for u in u]
-	#u = [stats.norm.rvs() for u in u]
+	#u = [stats.norm.rvs() for u in u] #not sure whether to use normal or uniform rv
 	f_x = [arccos((1-u)-1/float(2*pi)) for u in u]
-	#f_x = [arccos(2*pi*(1-u)-1) for u in u]
-	g_x = [2*pi*((1+cos(x))**(-2/float(3))) for x in f_x]
+	#f_x = [arccos(2*pi*(1-u)-1) for u in u] #not sure about the equation either - arccos evaluates to nan i.e., undefined
+	g_x = [2*pi*((1+cos(f_x))**(-2/float(3))) for f_x in f_x]
 	mu = mean(g_x)
 	writer.append("problem 2 mean is "+str(mu))
 
